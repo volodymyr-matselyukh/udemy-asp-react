5439a575343fdf5301764aaa3d1514d089b2feef
volodymyr.matseliukh <volodymyr.matselyukh@yandex.ru>
2022-03-16 00:25:01 +0200

    initial commit

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,29 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistance;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private DataContext _dbContext;
+
+        public ActivitiesController(DataContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _dbContext.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await _dbContext.Activities.FindAsync(id);
+        }
+    }
+}
