11be6af199ae9b4fceb1fbfd24ab476d225f5bb8
volodymyr.matseliukh <volodymyr.matselyukh@yandex.ru>
2022-03-20 01:13:58 +0200

    add client app to the project

--- clientapp/src/App.tsx
+++ clientapp/src/App.tsx
@@ -7,8 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-              <p style={{color: "red"}}>
-          Edit <code>src/App.tsx</code> and save to reload!
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
