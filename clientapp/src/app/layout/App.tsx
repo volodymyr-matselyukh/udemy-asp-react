e4e87641bc21d0b8b417dde72905e25fa6207356
volodymyr.matseliukh <volodymyr.matselyukh@yandex.ru>
2022-03-29 02:40:09 +0300

    adding dashboard and grid

---
+++ clientapp/src/app/layout/App.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
+import { Container, Header, List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+
+function App() {
+    const [activities, setActivities] = useState<Activity[]>([]);
+
+    useEffect(() => {
+        axios.get<Activity[]>('http://localhost:5000/api/activities')
+        .then(response => {
+            setActivities(response.data);
+        })
+    }, []);
+
+    return (
+        <>
+            <NavBar />
+
+            <Container style={{marginTop: "7em"}}>
+                <ActivityDashboard activities={activities}/>
+            </Container>
+            
+        </>
+    );
+}
+
+export default App;
