756eb343a312420a6da2e6b4ecae322118bc664f
volodymyr.matseliukh <volodymyr.matselyukh@yandex.ru>
2022-04-18 00:07:52 +0300

    beautify fe

--- clientapp/src/features/activities/dashboard/ActivityDashboard.tsx
+++ clientapp/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 import { Grid } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingComponents";
 import { useStore } from "../../../app/stores/store";
+import ActivityFilters from "./ActivityFilters";
 import ActivityList from "./ActivityList";
 
 export default observer(function ActivityDashboard() {
@@ -14,7 +15,7 @@ export default observer(function ActivityDashboard() {
     {
       loadActivities();
     }
-  }, [activityStore]);
+  }, [activityStore, loadActivities, activityRegistry.size]);
 
   if (activityStore.loadingInitial)
     return <LoadingComponent content="Loading app" />;
@@ -25,7 +26,7 @@ export default observer(function ActivityDashboard() {
         <ActivityList />
       </Grid.Column>
       <Grid.Column width="6">
-        <h2>Activities filter</h2>
+        <ActivityFilters />
       </Grid.Column>
     </Grid>
   );
